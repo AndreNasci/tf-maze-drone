92a0ed8a0c88b3114a74f525b7ab185aeaceb4f6
André Nascimento <andrenascimentofx@gmail.com>
2025-05-31 19:39:25 -0300

    Testing reprod, verifying dif top, and other tests.

--- script.sh
+++ script.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
-./maze-drone-v03/bin/python 6000_testing_reprod.py 2 1 1 1
-./maze-drone-v03/bin/python 6000_testing_reprod.py 2 1 1 2
-./maze-drone-v03/bin/python 6000_testing_reprod.py 2 1 1 3
-./maze-drone-v03/bin/python 6000_testing_reprod.py 2 1 1 4
-./maze-drone-v03/bin/python 6000_testing_reprod.py 2 1 1 5
+# ./maze-drone-v03/bin/python 9000_testing_reprod.py 2 1 1 1
+# ./maze-drone-v03/bin/python 9000_testing_reprod.py 2 1 1 2
+# ./maze-drone-v03/bin/python 9000_testing_reprod.py 2 1 1 3
+# ./maze-drone-v03/bin/python 9000_testing_reprod.py 2 1 1 4
+# ./maze-drone-v03/bin/python 9000_testing_reprod.py 2 1 1 5
+# #
+# ./maze-drone-v03/bin/python 9000_testing_reprod.py 2 2 1 1
+# ./maze-drone-v03/bin/python 9000_testing_reprod.py 2 2 1 2
+# ./maze-drone-v03/bin/python 9000_testing_reprod.py 2 2 1 3
+# ./maze-drone-v03/bin/python 9000_testing_reprod.py 2 2 1 4
+# ./maze-drone-v03/bin/python 9000_testing_reprod.py 2 2 1 5
 #
-./maze-drone-v03/bin/python 6000_testing_reprod.py 2 2 1 1
-./maze-drone-v03/bin/python 6000_testing_reprod.py 2 2 1 2
-./maze-drone-v03/bin/python 6000_testing_reprod.py 2 2 1 3
-./maze-drone-v03/bin/python 6000_testing_reprod.py 2 2 1 4
-./maze-drone-v03/bin/python 6000_testing_reprod.py 2 2 1 5
+./maze-drone-v03/bin/python 9002_testing_reprod_LR.py 3 3 5 1
+./maze-drone-v03/bin/python 9002_testing_reprod_LR.py 3 3 5 2
+./maze-drone-v03/bin/python 9002_testing_reprod_LR.py 3 3 5 3
+./maze-drone-v03/bin/python 9002_testing_reprod_LR.py 3 3 5 4
+./maze-drone-v03/bin/python 9002_testing_reprod_LR.py 3 3 5 5
 #
-./maze-drone-v03/bin/python 6000_testing_reprod.py 3 3 5 1
-./maze-drone-v03/bin/python 6000_testing_reprod.py 3 3 5 2
-./maze-drone-v03/bin/python 6000_testing_reprod.py 3 3 5 3
-./maze-drone-v03/bin/python 6000_testing_reprod.py 3 3 5 4
-./maze-drone-v03/bin/python 6000_testing_reprod.py 3 3 5 5
-#
-./maze-drone-v03/bin/python 6000_testing_reprod.py 3 4 5 1
-./maze-drone-v03/bin/python 6000_testing_reprod.py 3 4 5 2
-./maze-drone-v03/bin/python 6000_testing_reprod.py 3 4 5 3
-./maze-drone-v03/bin/python 6000_testing_reprod.py 3 4 5 4
-./maze-drone-v03/bin/python 6000_testing_reprod.py 3 4 5 5
+./maze-drone-v03/bin/python 9002_testing_reprod_LR.py 3 4 5 1
+./maze-drone-v03/bin/python 9002_testing_reprod_LR.py 3 4 5 2
+./maze-drone-v03/bin/python 9002_testing_reprod_LR.py 3 4 5 3
+./maze-drone-v03/bin/python 9002_testing_reprod_LR.py 3 4 5 4
+./maze-drone-v03/bin/python 9002_testing_reprod_LR.py 3 4 5 5
 
 
